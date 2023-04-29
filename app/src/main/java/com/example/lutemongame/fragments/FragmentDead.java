a6324710479a989300b13dc82d12928fb8ab9472
hylloo <106667418+hylloo@users.noreply.github.com>
2023-04-29 18:51:28 +0300

    poistettu tuhraa koodia

--- app/src/main/java/com/example/lutemongame/fragments/FragmentDead.java
+++ app/src/main/java/com/example/lutemongame/fragments/FragmentDead.java
@@ -1,9 +1,11 @@
 package com.example.lutemongame.fragments;
 
 import static com.example.lutemongame.Inventory.deadLutemons;
-import static com.example.lutemongame.Inventory.lutemons;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,18 +13,11 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.example.lutemongame.Inventory;
-import com.example.lutemongame.R;
 import com.example.lutemongame.DeadLutemonListAdapter;
+import com.example.lutemongame.R;
 
 
 public class FragmentDead extends Fragment {
-    private RecyclerView recyclerView;
-    private DeadLutemonListAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,9 +28,9 @@ public class FragmentDead extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Taking dead lutemons from the list and adding them to the recyclerview
-        recyclerView = view.findViewById(R.id.rvDeadLutemonList);
+        RecyclerView recyclerView = view.findViewById(R.id.rvDeadLutemonList);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter = new DeadLutemonListAdapter(getContext(), deadLutemons);
+        DeadLutemonListAdapter adapter = new DeadLutemonListAdapter(getContext(), deadLutemons);
         recyclerView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
     }
